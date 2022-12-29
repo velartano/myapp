e548fc31cb74303816da8a0ce47bf9eed27b225d
Tresor1223 <lysekonan3@gmail.com>
2022-12-29 13:15:22 +0100

    Création du form d'ajout

--- src/Controller/BienController.php
+++ src/Controller/BienController.php
@@ -3,104 +3,42 @@
 namespace App\Controller;
 
 use App\Entity\BienImmobilier;
-use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use App\Form\Type\BienType;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
 
 
-class BienController extends AbstractController{
-
-    public function createBien(ManagerRegistry $doctrine, Request $request): Response
+class BienController extends AbstractController
+{
+    /**
+     * @Route("/bien", name="app_bien")
+     */
+    public function index(ManagerRegistry $doctrine,Request $request): Response
     {
-        
         $entityManager = $doctrine->getManager();
-        //dd($request->query->get('titre')); // test
-
         $bien = new BienImmobilier();
-        $bien->setTitre($request->query->get('titre'));
-        $bien->setPrix($request->query->get('prix'));
-        $bien->setDescription($request->query->get('description'));
-        $bien->setville($request->query->get('ville'));
-        $bien->setCodePostal($request->query->get('code_postal'));
-        $bien->setSurface($request->query->get('surface'));
 
-        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-        $entityManager->persist($bien);
+        $form = $this->createForm(BienType::class, $bien);
+        $form->handleRequest($request);
+         if ($form->isSubmitted() && $form->isValid()) {
+            $bien = $form->getData();
+             // tell Doctrine you want to (eventually) save the Product (no queries yet)
+            $entityManager->persist($bien);
 
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
-
-        return new Response('Saved new bien with id '.$bien->getId());
-    }
-
-    public function updateBien(ManagerRegistry $doctrine, int $id, Request $request): Response
-    {
-       
-        $entityManager = $doctrine->getManager();
-        $bien = $entityManager->getRepository(BienImmobilier::class)->find($id);
-
-
-        if (!$bien) {
-            throw $this->createNotFoundException(
-                'No bien found for id '.$id
-            );
+            // actually executes the queries (i.e. the INSERT query)
+            $entityManager->flush();
+            return $this->redirectToRoute('app_bien');
         }
-        $titre = $request->query->get('titre');
-        $prix = $request->query->get('prix');
-        $description = $request->query->get('description');
-        $ville = $request->query->get('ville');
-        $codePostal = $request->query->get('code_postal');
-        $surface = $request->query->get('surface');
-
-        if( $titre != null && $titre != $bien->getTitre()) $bien->setTitre($titre);
-        if( $prix != null && $prix != $bien->getPrix()) $bien->setPrix($prix);
-        if( $description != null && $description != $bien->getDescription()) $bien->setDescription($description);
-        if( $ville != null && $ville != $bien->getville()) $bien->setville($ville);
-        if($codePostal != null && $codePostal != $bien->getCodePostal()) $bien->setCodePostal($codePostal);
-        if( $surface != null && $surface != $bien->getSurface()) $bien->setSurface($surface);
-
-        $entityManager->flush();
-
-        // return $this->redirectToRoute('bien_update', [
-        //     'id' => $bien->getId()
-        // ]);
-        return new Response('Modifications enregistrées');
+        
+        return $this->render('bien/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
-
-    public function removeBien(ManagerRegistry $doctrine, int $id): Response
-    {
-        $entityManager = $doctrine->getManager();
-        $bien = $entityManager->getRepository(BienImmobilier::class)->find($id);
-
-        if (!$bien) {
-            throw $this->createNotFoundException(
-                'No bien found for id '.$id
-            );
-        }
-
-        $entityManager->remove($bien);
-        $entityManager->flush();
-
-        // return $this->redirectToRoute('bien_show', [
-        //     'id' => $bien->getId()
-        // ]);
-        return new Response('Bien sypprimé');
-    }
-
-    public function new()
-    {
-        return $this ->render('bien.html.twig') ;
-    }
-
-    // public function testBase()
-    // {
-    //     return $this ->render('base.html.twig') ;
-    // }
-    
 }
-
-
-?>
---
+++ src/Form/Type/BienType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form\Type; 
+use Symfony\Component\Form\AbstractType; 
+use Symfony\Component\Form\Extension\Core\Type\DateType; 
+use Symfony\Component\Form\Extension\Core\Type\SubmitType; 
+use Symfony\Component\Form\Extension\Core\Type\TextType; 
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;  
+use Symfony\Component\Form\FormBuilderInterface; 
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class BienType extends AbstractType { 
+    
+    public function buildForm(FormBuilderInterface $builder, array $options): void 
+    { 
+        $builder
+       ->add('titre', TextType :: class)
+                 ->add('prix', IntegerType :: class)
+                 ->add('description', TextareaType :: class)
+                 ->add('ville', TextType :: class)
+                 ->add('code_postal', IntegerType :: class)
+                 ->add('surface', TextType :: class)
+                 ->add('Enregistrer', SubmitType :: class);
+    }
+
+
+}
